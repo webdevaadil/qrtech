02827f43ffb35cfbdafbc2c6c827fda9ae2bcdad
Ajay-at-005173903299 <anurag.pandey17@gmail.com>
2023-03-07 17:48:01 +0530

    final

--- index.js
+++ index.js
@@ -16,9 +16,17 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api/auth", require("./Router/Router"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 app.listen(5000, () => {
   console.log('server is running on 5000!');
